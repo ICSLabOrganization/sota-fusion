b811aa80599a0d19c233736a1756fb9529724ddd
VuseKaylous <vuvietkhanh1407@gmail.com>
2023-03-10 17:23:28 +0700

    config config.py

--- src/_config.py
+++ src/_config.py
@@ -1,11 +1,14 @@
 import yaml
 from pathlib import Path
 
-def load_config():
+def load_config(mode : str = None):
     CONFIG_PATH = Path(__file__).parent.parent.joinpath("config.yml")
 
     # read config file
     with open(CONFIG_PATH, "r") as config_file:
         config = yaml.safe_load(config_file)
+        if mode is None or config[mode] is None :
+            raise KeyError("A mode is not assigned in load config function, please re-check the called function and touch some grass")
 
-    return config
+
+    return config[mode]
