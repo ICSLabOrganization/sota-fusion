df561dddd5a7e8b62ca49c3ab977c5049cea7d24
tiendatAI <tiendatlework@gmail.com>
2023-03-27 16:39:44 +0700

    use another method for join_path

--- src/speech2image/recording.py
+++ src/speech2image/recording.py
@@ -1,19 +1,20 @@
-import sounddevice as sd
-from scipy.io.wavfile import write
+import sounddevice as sd #type: ignore
+from scipy.io.wavfile import write #type: ignore
 
 from pathlib import Path
 
-DIR_PATH = Path(__file__).parent.joinpath("speech-2-image")
-AUDIO_PATH = DIR_PATH.parent.joinpath("assets").joinpath("audio-test").joinpath("test1.wav")
+DIR_PATH = Path(__file__).parents[2] #root directory
+AUDIO_PATH = DIR_PATH.joinpath(*["assets", "audio-test", "test1.wav"])
 
 def recording():
     fs = 16000  # Sample rate 44100 --> fix to 16000
     seconds = 3  # Duration of recording
 
-    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    recording_obj = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
     sd.wait()  # Wait until recording is finished
-    write(AUDIO_PATH, fs, myrecording)  # Save as WAV file 
-    # return AUDIO_PATH
+    write(AUDIO_PATH, fs, recording_obj)  # Save as WAV file 
+
+    return AUDIO_PATH
 
 
 if __name__ == "__main__":
