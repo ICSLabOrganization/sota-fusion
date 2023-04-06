abdbcb0cf28834e608b880dd7c74b71329848a82
VuseKaylous <vuvietkhanh1407@gmail.com>
2023-04-06 21:04:15 +0700

    add style transfer model

---
+++ src/style_transfer/extras/run.sh
@@ -0,0 +1,5 @@
+python neural_style.py eval \
+    --content-image /home/kaylous/git_stuff/pytorch-neural-style-transfer/data/content-images/lion.jpg \
+    --model ./saved_models/candy.pth \
+    --output-image ./output/leone.jpg \
+    --cuda 0
